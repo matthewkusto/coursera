2e94573640720562eb555c583602f641e7ac00b3
matthewkusto <mkusto.2001@gmail.com>
2023-12-11 17:02:31 -0800

    spacing issues

--- cpp-memory/puzzle.cpp
+++ cpp-memory/puzzle.cpp
@@ -1,23 +1,25 @@
 /**
  * C++ code showing improper use of memory (returning a reference to a stack variable).
- * 
+ *
  * @author
  *   Wade Fagen-Ulmschneider <waf@illinois.edu>
  */
 
-double someOtherFunction();  // Forward decl
+double someOtherFunction(); // Forward decl
 
 #include <iostream>
 #include "Cube.h"
 using uiuc::Cube;
 
-Cube *CreateUnitCube() {
+Cube *CreateUnitCube()
+{
   Cube cube;
   cube.setLength(15);
   return &cube;
 }
 
-int main() {
+int main()
+{
   Cube *c = CreateUnitCube();
   someOtherFunction();
   double a = c->getSurfaceArea();
@@ -27,14 +29,15 @@ int main() {
   return 0;
 }
 
-
 // Some other function that does something that uses some stack memory.
 // In this code, we calculate the total volume of cubes of length from 0..99.
-double someOtherFunction() {
+double someOtherFunction()
+{
   Cube cubes[100];
   double totalVolume = 0;
 
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 100; i++)
+  {
     cubes[i].setLength(i);
     totalVolume += cubes[i].getVolume();
   }
